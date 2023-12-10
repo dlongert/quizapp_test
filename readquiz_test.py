73cce96a7d85c8340d464f12da30c8a9baa8ba55
somyanagar <somyanagar49@gmail.com>
2023-12-10 12:39:25 -0800

    Added setup and teardown

--- readquiz_test.py
+++ readquiz_test.py
@@ -8,22 +8,23 @@ from student.takequiz import Student
 from student.readquiz import Quiz
 from student.readquiz import Problem
 
-
 COURSE_CODE = 'DATA-531'
 READ_DATA = json.dumps([{"question":"Question 1","options":["A. A","B. B","C. C","D. D","E. E"],"answer":"A. A"}])
 
-options = ['A. A', 'B. B', 'C. C', 'D. D', 'E. E']
-problem = Problem("Question 1", options, 'A')
-quiz = Quiz(COURSE_CODE, [problem])
-student = Student('Student A', 12345)
 
-class ReadQuizTest(unittest.TestCase):
+class ReadQuizTest(unittest.TestCase):    
+        
+    def setUp(self):
+        self.options = ['A. A', 'B. B', 'C. C', 'D. D', 'E. E']
+        self.problem = Problem("Question 1", self.options, 'A')
+        self.quiz = Quiz(COURSE_CODE, [self.problem])
+        self.student = Student('Student A', 12345)
     
     def test_read_quiz(self):
         mock_open = mock.mock_open(read_data=READ_DATA)
         with mock.patch('builtins.open', mock_open):
             result = rq.read_quiz(COURSE_CODE)
-        self.assertEquals(result.name, quiz.name)
+        self.assertEquals(result.name, self.quiz.name)
         
     def test_get_available_courses(self):
         result = rq.get_available_courses()
@@ -32,12 +33,18 @@ class ReadQuizTest(unittest.TestCase):
     def test_save_score(self):
         mock_open = mock.mock_open(read_data=READ_DATA)
         with mock.patch('builtins.open', mock_open):
-            result = rq.save_score(student, COURSE_CODE, 10)
+            result = rq.save_score(self.student, COURSE_CODE, 10)
         self.assertEquals(result, True)
         
     def test_get_percentage(self):
         mock_open = mock.mock_open(read_data=READ_DATA)
         with mock.patch('builtins.open', mock_open):
-            result = rq.get_percentage(student)
+            result = rq.get_percentage(self.student)
         self.assertEquals(result, 0)
+        
+    def tearDown(self):
+        del self.student
+        del self.options
+        del self.quiz
+        del self.problem
 unittest.main(argv=[''], verbosity=2, exit=False)
--- takequiz_test.py
+++ takequiz_test.py
@@ -9,23 +9,23 @@ from student.readquiz import Problem
 
 COURSE_CODE = 'DATA-531'
 
-student = Student('Student A', 123)
 options = ['A. A', 'B. B', 'C. C', 'D. D', 'E. E']
 problem = Problem("Question 1", options, 'A')
 quiz = Quiz(COURSE_CODE, [problem])
 
-class ReadQuizTest(unittest.TestCase):
+class TakeQuizTest(unittest.TestCase):
+
+    def setUp(self):
+        self.student = Student('Student A', 123)
+        self.options = ['A. A', 'B. B', 'C. C', 'D. D', 'E. E']
+        self.problem = Problem("Question 1", options, 'A')
+        self.quiz = Quiz(COURSE_CODE, [problem])
     
     @mock.patch('student.takequiz.input', create=True)
     def test_student_handler(self, mocked_input):
         mocked_input.side_effect = ['Student A', 123]
         result = takequiz.student_handler()
-        self.assertEquals(result.student_number, student.student_number)
-
-    # @mock.patch('student.takequiz.input', create=True)
-    # def test_start_quiz(self, mocked_input):
-    #     mocked_input.side_effect = ['A']
-    #     self.assertEquals(takequiz.start_quiz(quiz), 100.0)
+        self.assertEquals(result.student_number, self.student.student_number)
 
     @mock.patch('student.takequiz.input', create=True)
     @mock.patch('student.readquiz.read_quiz', return_value = quiz)
@@ -34,6 +34,12 @@ class ReadQuizTest(unittest.TestCase):
     def test_select_quiz(self, mocked_input, mocked_read_quiz, mocked_save_score, mocked_get_percentage):
         mocked_input.side_effect = ['Student A', 123, 1, 1]
         self.assertEquals(takequiz.select_quiz(), None)
+    
+    def tearDown(self):
+        del self.student
+        del self.options
+        del self.quiz
+        del self.problem
         
 unittest.main(argv=[''], verbosity=2, exit=False)
 
