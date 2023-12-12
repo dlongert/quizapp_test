55a201300052880a03fafe6516bb2c10199d65c4
somyanagar <somyanagar49@gmail.com>
2023-12-11 22:24:53 -0800

    Added test suite

--- test_suite_quizapp.py
+++ test_suite_quizapp.py
@@ -2,6 +2,8 @@ import unittest
 
 from readquiz_test import ReadQuizTest
 from takequiz_test import TakeQuizTest
+from createquiz_test import CreateQuizTest
+from checkstudentscore_test import CheckStudentScoreTest
 
 def quizapp_test_suite():
     suite = unittest.TestSuite()
@@ -12,6 +14,15 @@ def quizapp_test_suite():
     suite.addTest(ReadQuizTest('test_get_percentage'))
     suite.addTest(TakeQuizTest('test_student_handler'))
     suite.addTest(TakeQuizTest('test_select_quiz'))
+    suite.addTest(CreateQuizTest("test_create_quiz"))
+    suite.addTest(CreateQuizTest("convert_to_json"))
+    suite.addTest(CreateQuizTest("create_score_csv"))
+    suite.addTest(CreateQuizTest("create_quiz"))
+    suite.addTest(CheckStudentScoreTest("test_get_student_scores"))
+    suite.addTest(CheckStudentScoreTest("test_set_student_scores"))
+    suite.addTest(CheckStudentScoreTest("test_quiz_score_statistics"))
+    suite.addTest(CheckStudentScoreTest("test_score_driver"))
+    suite.addTest(CheckStudentScoreTest("test_quiz_or_score"))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 quizapp_test_suite()
