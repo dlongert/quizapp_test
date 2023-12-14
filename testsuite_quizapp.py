97dad90c4e18432ced0c114d58dbc890e08daa4f
dlongert <longert520@gmail.com>
2023-12-14 08:29:39 -0800

    modifying test suite

--- testsuite_quizapp.py
+++ testsuite_quizapp.py
@@ -2,6 +2,8 @@ import unittest
 
 from createquiz_test import CreateQuizTest
 from checkstudentscore_test import CheckStudentScoreTest
+from readquiz_test import ReadQuizTest
+from takequiz_test import TakeQuizTest
 
 def quizapp_test_suite():
     suite = unittest.TestSuite()
@@ -10,12 +12,16 @@ def quizapp_test_suite():
     suite.addTest(CreateQuizTest("convert_to_json"))
     suite.addTest(CreateQuizTest("create_score_csv"))
     suite.addTest(CreateQuizTest("create_quiz"))
-    suite.addTest(CheckStudentScoreTest("test_get_student_scores"))
     suite.addTest(CheckStudentScoreTest("test_set_student_scores"))
     suite.addTest(CheckStudentScoreTest("test_quiz_score_statistics"))
     suite.addTest(CheckStudentScoreTest("test_score_driver"))
     suite.addTest(CheckStudentScoreTest("test_quiz_or_score"))
-
+    suite.addTest(ReadQuizTest('test_read_quiz'))
+    suite.addTest(ReadQuizTest('test_get_available_courses'))
+    suite.addTest(ReadQuizTest('test_save_score'))
+    suite.addTest(ReadQuizTest('test_get_percentage'))
+    suite.addTest(TakeQuizTest('test_student_handler'))
+    suite.addTest(TakeQuizTest('test_select_quiz'))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 quizapp_test_suite()
